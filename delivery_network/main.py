1343ec3447ed0e1044d4e793d0c16f659d55f156
tchen <thomas.chen@ensae.fr>
2023-03-25 12:42:45 +0000

    a

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,4 +1,4 @@
 """
 Ici on exécute le code (c'est le main.py du projet de programmation)
 """
-print("Salut à tous les amis c'est david lafarge pokemon")
+#print("Salut à tous les amis c'est david lafarge pokemon")
